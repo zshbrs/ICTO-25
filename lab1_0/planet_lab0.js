40b6f778afea184848e52bef767e960b4165f05e
zshbrs <zshbrs@gmail.com>
2025-06-13 00:03:13 +0300

    Update planet_lab0.js

--- lab1_0/planet_lab0.js
+++ lab1_0/planet_lab0.js
@@ -1,23 +1,22 @@
-const day = 24.0 * 60 * 60; // сек в добі
-const dt = day / 3;         // крок інтегрування (в секундах)
-const G = 6.67e-11;         // гравітаційна стала
-const scale = 1e3;          // 1000 км = 1 одиниця
+const day = 24.0 * 60 * 60; // тривалість земного дня у секундах
+const dt = day / 3; // крок інтегрування
+const G = 6.67e-11; // гравітаційна стала
 
 AFRAME.registerComponent('planet', {
   schema: {
-    name: { type: 'string', default: "" },
-    dist: { type: 'number', default: 0 },
-    mass: { type: 'number', default: 0 },
-    T: { type: 'number', default: 0 },
-    v: { type: 'array', default: [0, 0, 0] },
-    a: { type: 'array', default: [0, 0, 0] },
-    pos: { type: 'array', default: [0, 0, 0] }
+    name: { type: 'string', default: "" }, // ім'я планети
+    dist: { type: 'number', default: 0 }, // середня відстань від Сонця
+    mass: { type: 'number', default: 0 }, // маса
+    T: { type: 'number', default: 0 }, // планетарний рік у земних днях
+    v: { type: 'array', default: [0, 0, 0] }, // вектор швидкості
+    a: { type: 'array', default: [0, 0, 0] }, // вектор прискорення
+    pos: { type: 'array', default: [0, 0, 0] } // координатний вектор
   },
 
   init: function () {
     this.data.T *= day;
     this.data.pos[0] = this.data.dist;
-    this.el.setAttribute('position', (this.data.dist / scale) + ' 0 0');
+    this.el.setAttribute('position', (this.data.dist / 1e9) + ' 0 0');
 
     if (this.data.T !== 0) {
       this.data.v[1] = 2 * Math.PI * this.data.dist / this.data.T;
@@ -30,36 +29,43 @@ AFRAME.registerComponent('main', {
     this.solar_system = document.querySelectorAll('[planet]');
   },
 
-  tick: function () {
+  tick: function (time, deltaTime) {
     for (let i = 0; i < this.solar_system.length; i++) {
-      let pi = this.solar_system[i].getAttribute('planet');
-      pi.a = [0, 0, 0];
+      let planet_i = this.solar_system[i].getAttribute('planet');
+      planet_i.a = [0, 0, 0];
 
       for (let j = 0; j < this.solar_system.length; j++) {
-        if (i === j) continue;
-        let pj = this.solar_system[j].getAttribute('planet');
-        let dp = [0, 0, 0];
+        if (i !== j) {
+          let planet_j = this.solar_system[j].getAttribute('planet');
+          let deltapos = [0, 0, 0];
 
-        for (let k = 0; k < 3; k++) dp[k] = pj.pos[k] - pi.pos[k];
+          for (let k = 0; k < 3; k++) {
+            deltapos[k] = planet_j.pos[k] - planet_i.pos[k];
+          }
 
-        let r = Math.sqrt(dp[0] ** 2 + dp[1] ** 2 + dp[2] ** 2);
-        if (r === 0) continue;
+          let r = Math.sqrt(
+            deltapos[0] ** 2 + deltapos[1] ** 2 + deltapos[2] ** 2
+          );
 
-        for (let k = 0; k < 3; k++) {
-          pi.a[k] += (G * pj.mass * dp[k]) / Math.pow(r, 3);
+          if (r !== 0) {
+            for (let k = 0; k < 3; k++) {
+              planet_i.a[k] += (G * planet_j.mass * deltapos[k]) / Math.pow(r, 3);
+            }
+          }
         }
       }
 
       for (let k = 0; k < 3; k++) {
-        pi.v[k] += pi.a[k] * dt;
-        pi.pos[k] += pi.v[k] * dt;
+        planet_i.v[k] += planet_i.a[k] * dt;
+        planet_i.pos[k] += planet_i.v[k] * dt;
       }
 
       this.solar_system[i].setAttribute(
         'position',
-        (pi.pos[0] / scale) + ' ' + (pi.pos[1] / scale) + ' ' + (pi.pos[2] / scale)
+        (planet_i.pos[0] / 1e9) + ' ' + (planet_i.pos[1] / 1e9) + ' ' + (planet_i.pos[2] / 1e9)
       );
     }
   }
 });
+
 
