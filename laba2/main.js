9559aa7b78962f22690dd0143d9d206256fe9e55
zshbrs <zshbrs@gmail.com>
2025-06-15 19:05:08 +0300

    Update main.js

--- laba2/main.js
+++ laba2/main.js
@@ -1,8 +1,5 @@
-import * as THREE from 'three';
-import { MindARThree } from 'mind-ar/dist/mindar-image-three.prod.js';
-
 const start = async () => {
-  const mindarThree = new MindARThree({
+  const mindarThree = new window.MINDAR.IMAGE.MindARThree({
     container: document.querySelector("#ar-container"),
     imageTargetSrc: "./assets/targets.mind",
   });
@@ -10,16 +7,16 @@ const start = async () => {
   const { renderer, scene, camera } = mindarThree;
 
   const light = new THREE.PointLight(0xffffff, 1.5, 0);
-  light.position.set(30, 0, 0); // 30 "тис." км умовно
+  light.position.set(30, 0, 0);
   scene.add(light);
 
   const anchor = mindarThree.addAnchor(0);
   const group = new THREE.Group();
   anchor.group.add(group);
 
-  const textureLoader = new THREE.TextureLoader();
-  const earthTexture = textureLoader.load('./assets/2k_earth_daymap.jpg');
-  const moonTexture = textureLoader.load('./assets/2k_moon.jpg');
+  const loader = new THREE.TextureLoader();
+  const earthTexture = loader.load('./assets/2k_earth_daymap.jpg');
+  const moonTexture = loader.load('./assets/2k_moon.jpg');
 
   const earthRadius = 0.064;
   const moonRadius = 0.0174;
@@ -46,13 +43,11 @@ const start = async () => {
   renderer.setAnimationLoop(() => {
     const time = performance.now() / 1000;
 
-    // 1 оберт Землі = 1 доба => оберт за 1/17000 секунди
     earth.rotation.y = time * (2 * Math.PI / (1 / 17000));
-
-    // 1 оберт Місяця навколо Землі = 28 діб => за 28/17000 секунди
     moonOrbit.rotation.y = time * (2 * Math.PI / (28 / 17000));
 
     renderer.render(scene, camera);
   });
 };
+
 start();
